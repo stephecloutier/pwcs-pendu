37c85b450c8073a6f742316af637e6926757763e
stephecloutier <stephecloutier@gmail.com>
2017-03-01 21:27:41 +0100

    Removed const. datas_dir from index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 define('CONFIGS_DIR', __DIR__.'/configs');
 define('CONTROLLERS_DIR', __DIR__.'/controllers');
 define('VIEWS_DIR', __DIR__.'/views');
-define('DATAS_DIR', __DIR__.'/datas');
 
 set_include_path(get_include_path().PATH_SEPARATOR.CONFIGS_DIR.PATH_SEPARATOR.CONTROLLERS_DIR.PATH_SEPARATOR.VIEWS_DIR);
 
